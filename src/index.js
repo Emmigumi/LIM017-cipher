9bcf285875be6453714b1d193d5ac9bddf58bf6f
Emmigumi <20160133@lamolina.edu.pe>
2022-02-03 23:58:18 -0500

    Mi second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+const btn1 = document.getElementById('btn1');
+btn1.addEventListener('click', () => {
+    alert("Gracias por su amor y compromiso con el prójimo");
+    document.getElementById("section1").style.display="none";
+    document.getElementById("section2").style.display="block";
+
+  });
+
+  const empezar = () => {
+    document.getElementById("root").className = "page1";
+    document.getElementById("section1").className = "page1";
+    
+    };
+    
+  //var string= document.getElementById("message").charCodeAt() 
