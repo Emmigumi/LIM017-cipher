5edcd0cabb599dd96417299ebc03b0f282a0f580
Emmigumi <20160133@lamolina.edu.pe>
2022-02-15 10:44:21 -0500

    Mi tercer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,26 @@
 const cipher = {
-  // ...
+  encode: (offset, string) => {
+    for (let i= 0; i<(string.length); i++) {
+      let asciiPosition = string.charCodeAt(i);
+      let enCodexString = (asciiPosition-65 + offset) %26 + 65;
+      let result = String.fromCharCode(enCodexString);
+    };
+    let desplazamiento= document.getElementById("offset1");
+    return result;
+    }, 
+  decode: (offset, string) => {
+    for (let i= 0; i<(string.length); i++) {
+      let asciiPosition = string.charCodeAt(i);
+      let enCodexString = (asciiPosition-65 + offset) %26 + 65;
+      let result = String.fromCharCode(enCodexString);
+      
+    };
+    let desplazamiento = document.getElementById("ofsset1");
+    return result;
+  },
 };
 
+  //decode()
+
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -5,15 +5,27 @@ console.log(cipher);
 const btn1 = document.getElementById('btn1');
 btn1.addEventListener('click', () => {
     alert("Gracias por su amor y compromiso con el prójimo");
-    document.getElementById("section1").style.display="none";
-    document.getElementById("section2").style.display="block";
-
+    document.getElementById("ONROUP").style.display="none";
+    document.getElementById("section1").style.display="block";
+    
+    var nombre = document.getElementById("name").value
+    document.getElementById("greet").innerHTML = "Hola " + nombre;
   });
 
-  const empezar = () => {
-    document.getElementById("root").className = "page1";
-    document.getElementById("section1").className = "page1";
-    
-    };
-    
-  //var string= document.getElementById("message").charCodeAt() 
+  const string= document.getElementById("message1");
+
+
+    const bot1 = document.getElementById("bnt2");
+    bot1.addEventListener('click',( ) => {
+      const cifrador = cipher.encode;
+console.log(cifrador);
+      return cifrador;
+    });
+
+    const bot2 = document.getElementById("bnt3");
+    bot2.addEventListener('click',( ) => {
+      const descifrador = cipher.decode;
+      return descifrador;
+    });
+ 
+ //addEventListener("click",//FUNCION)
